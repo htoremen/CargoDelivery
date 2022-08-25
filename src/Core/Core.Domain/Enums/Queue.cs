4db673b7e85622355a2f55951f5d1a48c93d586b
Hüseyin TÖREMEN <98386762+huseyin-toremen-safrantek@users.noreply.github.com>
2022-08-25 18:01:16 +0300

    Added Queue

---
+++ src/Core/Core.Domain/Enums/Queue.cs
@@ -0,0 +1,22 @@
+﻿namespace Core.Domain.Enums;
+
+public enum Queue
+{
+    None = 0,
+    CreateOrder = 1,
+    CreateSelfie = 2,
+    OrderApproved = 3,
+    OrderRejected = 4,
+
+    RouteConfirmed = 5,
+    AutoRoute = 6,
+    ManuelRoute = 7,
+    RouteCreated = 8,
+
+    NotDelivered = 9,
+    CreateRefund = 10,
+    TakeSelfei = 11,
+    CargoRefundCompleted = 12,
+
+    DisributionComplete=13,
+}
