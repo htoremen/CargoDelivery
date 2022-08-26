4f342f972139ec73630184eebe3601d76d98b180
Hüseyin TÖREMEN <98386762+huseyin-toremen-safrantek@users.noreply.github.com>
2022-08-26 11:25:19 +0300

    Updated Queue

--- src/Core/Core.Domain/Enums/Queue.cs
+++ src/Core/Core.Domain/Enums/Queue.cs
@@ -3,20 +3,44 @@
 public enum Queue
 {
     None = 0,
-    CreateOrder = 1,
-    CreateSelfie = 2,
-    OrderApproved = 3,
-    OrderRejected = 4,
 
-    RouteConfirmed = 5,
-    AutoRoute = 6,
-    ManuelRoute = 7,
-    RouteCreated = 8,
+    // Zimmetine Geçir
+    CreateOrder = 101,
+    CreateSelfie = 102,
+    OrderApproved = 103,
+    OrderRejected = 104,
 
-    NotDelivered = 9,
-    CreateRefund = 10,
-    TakeSelfei = 11,
-    CargoRefundCompleted = 12,
+    // Güzergah Oluştur
+    RouteConfirmed = 201,
+    AutoRoute = 202,
+    ManuelRoute = 203,
+    RouteCreated = 204,
 
-    DisributionComplete=13,
+    // Kargo İşlemleri
+
+    // Teslim Edilemedi
+    NotDelivered = 301,
+    CreateRefund = 302,
+    TakeSelfei = 303,
+    CargoRefundCompleted = 304,
+
+    // İade Oluştur
+    SendDamageReport = 311,
+    DamageRecordCompletion = 312,
+
+    // Teslimat Oluştur
+    CreateDelivery = 321,
+    // Kredi Kartı
+    CardPayment = 331,
+    CardPaymentCompleted = 332,
+    // Manuel Ödeme
+    PayAtDoor = 341,
+    PayAtDoorCompleted = 342,
+    // Teslimat İptal
+    RollBack = 351,
+    CancelDelivered = 352,
+
+    // Kargo İşlemleri Ortak
+    DisributionCheck = 361,
+    ShiftCompletion = 362,
 }
