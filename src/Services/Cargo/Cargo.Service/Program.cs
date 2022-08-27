4897ce8166b604d0e7ec88aa75984924dc9501f9
Hüseyin TÖREMEN <98386762+huseyin-toremen-safrantek@users.noreply.github.com>
2022-08-27 20:13:34 +0300

    fix

---
+++ src/Services/Cargo/Cargo.Service/Program.cs
@@ -0,0 +1,24 @@
+using Cargo.Service;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices();
+builder.Services.AddWebUIServices();
+builder.Services.AddEventBus(builder.Configuration);
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
