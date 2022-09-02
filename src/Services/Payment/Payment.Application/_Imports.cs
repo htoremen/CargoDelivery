c579968c1c022b609b1dee8fb12d4bfa744af9b0
Hüseyin TÖREMEN <98386762+huseyin-toremen-safrantek@users.noreply.github.com>
2022-09-02 23:13:29 +0300

    Added Payment.Service

---
+++ src/Services/Payment/Payment.Application/_Imports.cs
@@ -0,0 +1,8 @@
+﻿global using Core.Domain;
+global using Bus;
+global using Events;
+global using MediatR;
+global using Cargos;
+global using Routes;
+global using Core.Domain.Enums;
+global using Core.Domain.Models;
---
+++ src/Services/Payment/Payment.Infrastructure/_Imports.cs
@@ -0,0 +1,4 @@
+﻿global using Payment.Application.Common.Interfaces;
+global using Payment.Application.Common.Models;
+global using Payment.Infrastructure.Identity;
+global using Payment.Infrastructure.Services;
