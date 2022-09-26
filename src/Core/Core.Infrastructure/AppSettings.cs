de805635767f8f4b331aafb4b741da9d9d546706
Hüseyin TÖREMEN <98386762+huseyin-toremen-safrantek@users.noreply.github.com>
2022-09-26 13:10:03 +0300

    Added ServiceUrls

--- src/Core/Core.Infrastructure/AppSettings.cs
+++ src/Core/Core.Infrastructure/AppSettings.cs
@@ -8,6 +8,8 @@ public class AppSettings
     public MessageBrokerOptions MessageBroker { get; set; }
     public ConnectionStrings ConnectionStrings { get; set; }
     public MongoDbSettings MongoDbSettings { get; set; }
+
+    public ServiceUrls ServiceUrls { get; set; }
 }
 
 
@@ -17,4 +19,13 @@ public class ConnectionStrings
     public string CargoConnectionString { get; set; }
     public string RouteConnectionString { get; set; }
     public string DeliveryConnectionString { get; set; }
+}
+
+public class ServiceUrls
+{
+    public string Saga { get; set; }
+    public string Cargo { get; set; }
+    public string Route { get; set; }
+    public string Delivery { get; set; }
+    public string Payment { get; set; }
 }
